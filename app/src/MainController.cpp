a84e72f593e6409dac0e1a95f43894a382d2e60e
zpantovic <zaga123.zp@gmail.com>
2025-10-08 15:50:08 +0200

    add draw_babyoda, but still cannot see model

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -5,6 +5,8 @@
 #include "MainController.h"
 
 #include "engine/core/Controller.hpp"
+#include "engine/graphics/GraphicsController.hpp"
+#include "engine/graphics/OpenGL.hpp"
 #include "engine/platform/PlatformController.hpp"
 #include "engine/resources/ResourcesController.hpp"
 #include "spdlog/spdlog.h"
@@ -13,6 +15,7 @@ namespace app {
 
     void MainController::initialize() {
         spdlog::info("MainController initialized");
+        engine::graphics::OpenGL::enable_depth_testing();
     }
 
     bool MainController::loop() {
@@ -24,23 +27,36 @@ namespace app {
         }
 
     void MainController::begin_draw() {
-
+        engine::graphics::OpenGL::clear_buffers();
     }
     void MainController::draw_babyoda() {
         // model
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
         auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
-        engine::resources::Model * model = resources->model("babyoda3");
+        engine::resources::Model * babyoda = resources->model("babyoda3");
 
         // shader
         engine::resources::Shader* shader = resources->shader("basic");
 
-        model->draw(shader);
+
+        shader->use();
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", graphics->camera()->view_matrix());
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::scale(model, glm::vec3(0.3f));
+        shader->set_mat4("model", model);
+
+        babyoda->draw(shader);
     }
     void MainController::draw() {
+        //clear buffers color buffer i depth buffer
         draw_babyoda();
+        //swapBuffers, kako bi sve sto smo nacrtali poslali na ekran.
     }
     void MainController::end_draw() {
-
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->swap_buffers();
     }
 
 
